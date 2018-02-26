dd5264fae9045e981c33b7765590bdbba93ee5c9
Hemil96 <hemilpatel41196@gmail.com>
2018-02-26 12:11:47 +0530

    redeveloped

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
-import requests
-import json
+import requests, json
 #Lists required in data structure
 list_app_data = []
 list_utilities = []
@@ -8,64 +7,54 @@ list_devops = []
 list_b_tools = []
 
 #Response data structure
-data = {
-        "Application and Data" : list_app_data,
-        "Utilities":list_utilities,
-        "DevOps":list_devops,
-        "Business Tools":list_b_tools
-        }
+dic = {}
 
 #scrapping function
 def techinside(company):
-    del list_app_data[:]
+   del list_app_data[:]
     del list_utilities[:]
     del list_devops[:]
     del list_b_tools[:]
+    dic.clear()
     try: #exception handling
         #getting html data into a variable named soup
         html_doc = requests.get("https://stackshare.io/"+company).text
         soup = BeautifulSoup(html_doc, 'html.parser')
-
         try:
-            #searching for particular div
-            all_divs = soup.find_all("div", {"class": "show-layer-d"})
+             #searching for particular div
+             all_divs = soup.find_all("div", {"class": "show-layer-d"})
         except:
-            return "Not found"
+             return ("N/A")
 
         #categorizing data
         for div in all_divs:
             if "Application and Data" in div.text:
                 app_data = div
-                print app_data
+                for a in app_data.find_all('a',{"data-hint":True}):
+                    list_app_data.append(a['data-hint'])
             elif "Utilities" in div.text:
                 utilities = div
+                for a in utilities.find_all('a',{"data-hint":True}):
+                    list_utilities.append(a['data-hint'])
+
             elif "DevOps" in div.text:
                 devops = div
+                for a in devops.find_all('a',{"data-hint":True}):
+                    list_devops.append(a['data-hint'])
+
             elif "Business Tools" in div.text:
                 b_tools = div
-        #appending data
-        try:
-            for a in app_data.find_all('a'):
-                list_app_data.append(a['data-hint'])
-        except:
-            pass
-        try:
-            for a in utilities.find_all('a'):
-                list_utilities.append(a['data-hint'])
-        except:
-            pass     
-        try:
-            for a in devops.find_all('a'):
-                list_devops.append(a['data-hint'])
-        except:
-            pass     
-        try:
-            for a in b_tools.find_all('a'):
-                list_b_tools.append(a['data-hint'])
-        except:
-            pass
-        #Response
-        return json.dumps(data)#json.dumps dump the python dict to json
-    except:
-        return "Not found"
+                for a in b_tools.find_all('a',{"data-hint":True}):
+                    list_b_tools.append(a['data-hint'])
 
+        #making response dict
+        if len(list_app_data)!=0:dic["app_data"] = list_app_data
+        if len(list_utilities)!=0:dic["utilitites"] = list_utilities
+        if len(list_devops)!=0:dic["devops"] = list_devops
+        if len(list_b_tools)!=0:dic["b_tools"] = list_b_tools
+        if len(dic)!= 0:
+            return json.dumps(dic)
+        else:
+            return ("N/A")
+    except:
+        return ("N/A")
